3e1465e7c9e2af15a78ed59121288d96e37b1175
Kevin <kevin@kevin-ThinkPad-T420.(none)>
2012-03-07 16:37:30 -0500

    added mean of means procedure, started on standard deviation procedure

--- dice.rkt
+++ dice.rkt
@@ -55,4 +55,29 @@
 		(let ((n (length trial-body)))
 			(/ (accumlate aux trial-body) n))))
 
+;; this takes the mean of means of multiple trials, in this context, multiple domains 
+(define (mean-of-means trial list-of-trials)
+	(let ((n (length list-of-trials)))
+	(define (aux list-of-trials counter)
+		(if (null? list-of-trials)
+		    (/ counter n)
+		   (aux (cdr list-of-trials) (+ counter (trial (car list of trials))))))
+	(aux list-of-trials 0)))
 
+;; this calculates the standard deviation of one trial, that is, of one domain
+(define (standard-deviation atrial-body)
+	(let ((m (trial atrial-body)))
+		(define (list-gen alist)
+			(if (null? alist) '()
+			    (cons (diceceoff (car (car alist))
+					    (car (cdr (car alist))))
+				(list-gen alist))))
+		(define (scnd-list-gen alist)
+			(if (null? alist) '()
+			   (cons (square (- m (car alist)))
+                                 (scnd-list-gen (cdr alist)))))
+
+
+			     
+	
+	

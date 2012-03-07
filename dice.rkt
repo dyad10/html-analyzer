7fc8c041f7bff08c0768130e5aa4cf1f16c61db3
Kevin <kevin@kevin-ThinkPad-T420.(none)>
2012-03-07 16:09:38 -0500

    cleaned up the code a bit

--- dice.rkt
+++ dice.rkt
@@ -1,35 +1,4 @@
 #lang racket
-(define (rawnumer shorter longer)
-  (define (aux shorter longer counter)
-    (cond ((null? shorter) counter)
-          ((eq? (car shorter)
-                (car longer))
-           (aux (cdr shorter)
-                (cdr longer)
-                (+ counter 1)))
-          (else (aux (cdr shorter)
-                     (cdr longer)
-                     counter))))
-  (if (< (length shorter)
-         (length longer))
-      (aux shorter longer 0)
-      (aux longer shorter 0)))
-
-(define (dicenumer alist blist)
-  (let ((x  (rawnumer alist blist)))
-    (* x 2)))
-
-(define (dicecoeff alist blist)
-  (/ (dicenumer alist blist)
-    (* 2 (length alist))))
-
-(define (lister astring)
-(let ((x (string-length astring)))
-  (define (aux counter)
-    (if (= x counter) '()
-        (cons (string-ref astring counter)
-              (aux (+ counter 1)))))
-  (aux 0)))
 
 (define (accumlate proc alist)
 	(define (aux proc alist counter)
@@ -38,21 +7,52 @@
 		    	      	   	     (proc (car alist))))))
 		(aux proc alist 0))
 
-(define (multi-list-test test test-body)
-	(define (aux test test-body counter)
-		(if (null? test-body) counter
-		    (aux test (cons (cadr test-body)
-		    	      	    (cdr test-body))
-				    (+ counter
-		    	      	       (test (car test-body)
-					     (cadr test-body))))))
-		(aux test test-body 0))
 
-(define (dice-trial dice-test test-body)
-	(/ (mult-list-test dice-test test-body)
-	   (length test-body)))
+(define (dicecoeff alist blist)
+	(define (rawnumer shorter longer)
+  		(define (aux shorter longer counter)
+    			(cond ((null? shorter) counter)
+          		((eq? (car shorter)
+                	      (car longer))
+          			 (aux (cdr shorter)
+                		      (cdr longer)
+                		      (+ counter 1)))
+          		(else (aux (cdr shorter)
+                     		   (cdr longer)
+                     		    counter))))
+  	(if (< (length shorter)
+         	(length longer))
+      	    (aux shorter longer 0)
+            (aux longer shorter 0)))
+	(define (dicenumer alist blist)
+  		(let ((x  (rawnumer alist blist)))
+    		(* x 2)))
+  (/ (dicenumer alist blist)
+    (* 2 (length alist))))
+
+(define (trial-creator base-list)
+	(define (aux appendage base-list)
+	(if (null? base-list)'()
+	    (cons (list appendage 
+	    	  	(car base-list))
+		(aux appendage (cdr base-list)))))
+	(define (dblaux xlist base-list)
+	(if (null? xlist) '()
+	    (cons (aux (car xlist) base-list)
+	    	  (dblaux (cdr xlist) base-list))))
+	(let ((xlist base-list))
+	     (dblaux xlist base-list)))
+
+(define trial
+	(lambda (trial-body)
+		(define (first-aux test-body counter)
+			(if (null? test-body) counter
+			   (first-aux (cdr test-body) 
+			              (+ counter (dicecoeff (car (car test-body)) 
+							(car (cdr (car test-body))))))))
+		(define (aux test-body)
+			(/ (first-aux test-body 0) (length test-body)))
+		(let ((n (length trial-body)))
+			(/ (accumlate aux trial-body) n))))
 
 
-
-
- 
